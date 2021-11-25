916330c501864e4b48f2d840ba289e43145f7d2f
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2021-11-25 01:24:14 -0500

    Furthered fleshed out script

--- scripts/processing/initialize_processing.py
+++ scripts/processing/initialize_processing.py
@@ -12,17 +12,34 @@
 
 import definitions
 import os
+import snap
+import sys
 
-from network_processing import collect_ids, random_select
-from scripts.utility import write_output
+from network_processing import collect_ids, random_select, create_network
+from scripts.utility import read_file, write_file
+from scripts.utility import PROC_USAGE
+
+if (len(sys.argv) != 2):
+    print("ERROR!")
+    print(PROC_USAGE)
+    sys.exit(-1)
 
 # Creating output folder if one doesn't already exist
 if (not os.path.isdir(definitions.OUTPUT_PATH)):
     os.makedirs(definitions.OUTPUT_PATH)
 
-id_path = collect_ids()
-node_list = random_select(id_path)
-
+arg = sys.argv[1]
 node_path = definitions.OUTPUT_PATH + "\\" + "selected_nodes.txt"
 
-write_output(node_list, node_path)
+# Generate list of random ego nodes for processing
+if (arg == "-egenrate"):
+
+    id_path = collect_ids()
+    node_list = random_select(id_path)
+    write_file(node_list, node_path)
+
+elif (arg == "-process"):
+
+    data = read_file(node_path)
+
+    net = create_network(data[0])
